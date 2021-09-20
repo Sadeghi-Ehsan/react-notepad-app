42e067d939ffa8db227cc160be7ffac314db8bf6
Ehsan <sadeghii.ehsan@gmail.com>
2021-09-21 00:28:42 +0430

    step 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function Note({ notePad, index, removeNote }) {
+    return (
+        <div className="" >
+            <form >
+                <input type="text" defaultValue={notePad.title} />
+                <textarea  rows="4" cols="30" defaultValue={notePad.text}/>
+                <button onClick={() => removeNote(index)}>delete</button>
+            </form>
+        </div>
+    );
+}
+
+function NoteForm({ addNote }) {
+    const [value, setValue] = React.useState("");
+
+    const addNotePad = () => {
+        if (!value) return;
+        addNote(value);
+        setValue("");
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                placeholder={'My Note Pad Title'}
+                onChange={e => setValue(e.target.value)}
+            />
+            <div>
+                <button onClick={() => addNotePad()}>Save</button>
+                <button onClick={() => console.log('delete')}>Delete</button>
+            </div>
+        </div>
+
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [notes, setNotes] = React.useState([
+        {
+            title: "Title Of first note",
+            text: "first note description",
+        },
+        {
+            title: "Title Of second note",
+            text: "second note description",
+        },
+
+    ]);
+
+    const addNote = text => {
+        const newNotes = [...notes, { text }];
+        setNotes(newNotes);
+    };
+
+    const removeNote = index => {
+        const newNotes = [...notes];
+        newNotes.splice(index, 1);
+        setNotes(newNotes);
+    };
+
+    return (
+        <div className="app">
+            <div className="notePad-list">
+                <NoteForm addNote={addNote} />
+                {notes.map((notePad, index) => (
+                    <Note
+                        key={index}
+                        index={index}
+                        notePad={notePad}
+                        removeNote={removeNote}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
