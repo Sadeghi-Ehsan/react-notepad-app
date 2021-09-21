6184544643569e4ca7a659178915d827b7285341
Administrator <sadeghii.ehsan@gmail.com>
2021-09-21 10:19:49 +0430

    built in webpack added! sass loader , some major changes

--- src/App.js
+++ src/App.js
@@ -1,89 +1,89 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
-function Note({ notePad, index, removeNote }) {
-    return (
-        <div>
-            <div className="container">
-                <form>
-                    <div className="form-group">
-                        <input className="form-control" type="text" defaultValue={notePad.title}/>
-                        <textarea className="form-control" rows="4" cols="30" defaultValue={notePad.text}/>
-                        <button className="form-control" onClick={() => removeNote(index)}>delete</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+function Note({notePad, index, removeNote}) {
+  return (
+    <div>
+      <div className="container">
+        <form>
+          <div className="form-group">
+            <input className="form-control" type="text" defaultValue={notePad.title}/>
+            <textarea className="form-control" rows="4" cols="30" defaultValue={notePad.text}/>
+            <button className="form-control" onClick={() => removeNote(index)}>delete</button>
+          </div>
+        </form>
+      </div>
+    </div>
 
-    );
+  );
 }
 
-function NoteForm({ addNote }) {
-    const [value, setValue] = React.useState("");
+function NoteForm({addNote}) {
+  const [value, setValue] = React.useState("");
 
-    const addNotePad = () => {
-        if (!value) return;
-        addNote(value);
-        setValue("");
-    };
+  const addNotePad = () => {
+    if (!value) return;
+    addNote(value);
+    setValue("");
+  };
 
-    return (
-        <div>
-            <input
-                type="text"
-                className="input"
-                value={value}
-                placeholder={'My Note Pad Title'}
-                onChange={e => setValue(e.target.value)}
-            />
-            <div>
-                <button onClick={() => addNotePad()}>Save</button>
-                <button onClick={() => console.log('delete')}>Delete</button>
-            </div>
-        </div>
+  return (
+    <div>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder={'My Note Pad Title'}
+        onChange={e => setValue(e.target.value)}
+      />
+      <div>
+        <button onClick={() => addNotePad()}>Save</button>
+        <button onClick={() => console.log('delete')}>Delete</button>
+      </div>
+    </div>
 
-    );
+  );
 }
 
 function App() {
-    const [notes, setNotes] = React.useState([
-        {
-            title: "Title Of first note",
-            text: "first note description",
-        },
-        {
-            title: "Title Of second note",
-            text: "second note description",
-        },
+  const [notes, setNotes] = React.useState([
+    {
+      title: "Title Of first note",
+      text: "first note description",
+    },
+    {
+      title: "Title Of second note",
+      text: "second note description",
+    },
 
-    ]);
+  ]);
 
-    const addNote = text => {
-        const newNotes = [...notes, { text }];
-        setNotes(newNotes);
-    };
+  const addNote = text => {
+    const newNotes = [...notes, {text}];
+    setNotes(newNotes);
+  };
 
-    const removeNote = index => {
-        const newNotes = [...notes];
-        newNotes.splice(index, 1);
-        setNotes(newNotes);
-    };
+  const removeNote = index => {
+    const newNotes = [...notes];
+    newNotes.splice(index, 1);
+    setNotes(newNotes);
+  };
 
-    return (
-        <div className="app">
-            <div className="notePad-list">
-                <NoteForm addNote={addNote} />
-                {notes.map((notePad, index) => (
-                    <Note
-                        key={index}
-                        index={index}
-                        notePad={notePad}
-                        removeNote={removeNote}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="app">
+      <div className="notePad-list">
+        <NoteForm addNote={addNote}/>
+        {notes.map((notePad, index) => (
+          <Note
+            key={index}
+            index={index}
+            notePad={notePad}
+            removeNote={removeNote}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path:path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test:  /\.(sass|scss)$/,
+        use: ["style-loader",
+          "css-loader",
+          "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+
+}
